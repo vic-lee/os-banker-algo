c1312b9e73e5c1c0c9c3d784ae64b6628251dd8c
vic-lee <viclee@nyu.edu>
2019-04-03 19:28:33 -0400

    Rename file

---
+++ src/ds/activity_terminate.h
@@ -0,0 +1,19 @@
+#ifndef HEADER_ACTIVITY_TERMINATION
+#define HEADER_ACTIVITY_TERMINATION
+
+#include <string>
+#include "activity.h"
+
+namespace task
+{
+
+class Termination : public Activity
+{
+  public:
+    Termination(int target_id, int delay) 
+    : Activity("terminate", target_id, delay) {}
+};
+
+} // namespace task
+
+#endif

4bfacb8b1b400a7243aebab193760def45412730
vic-lee <viclee@nyu.edu>
2019-04-03 17:57:36 -0400

    Rename folder

---
+++ src/ds/task.h
@@ -0,0 +1,21 @@
+#ifndef HEADER_TASK
+#define HEADER_TASK
+
+#include <string>
+#include <map>
+#include <vector> 
+
+#include "resource.h"
+#include "activity.h"
+
+namespace task
+{
+class Task
+{
+    std::string id;
+    std::map<int, Resource> claims_table;
+    std::vector<Activity> activities_table;
+};
+} // namespace Task
+
+#endif

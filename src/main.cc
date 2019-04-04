00fc3b99ba0c72a94ccd01c850febc77a609bed9
vic-lee <viclee@nyu.edu>
2019-04-04 18:00:07 -0400

    Add function to process user input

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,6 @@
 #include <string>
 #include <iostream>
+#include <cctype>
 
 #include "io/taskreader.h"
 #include "ds/tasktable.h"
@@ -7,17 +8,29 @@
 namespace banker
 {
 
+std::string read_cmd_arg(int argc, char **argv)
+{
+    std::string input_fpath = "test/input-01";
+
+    if (argc > 1)
+    {
+        std::string input = argv[1];
+        if (input.find("test") != std::string::npos)
+            input_fpath = input;
+        else
+            input_fpath = "test/input-0" + input;
+    }
+
+    return input_fpath;
 }
+
+} // namespace banker
 
 int main(int argc, char **argv)
 {
-    std::string input_fpath = "test/input-01";
-    
-    if (argc > 1)
-        input_fpath = argv[1];
-
+    std::string input_fpath = banker::read_cmd_arg(argc, argv);
     io::TaskReader task_reader(input_fpath);
     task::TaskTable task_table = task_reader.to_tasktable();
 
     return 0;
-}   
+}

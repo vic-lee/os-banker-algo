2b3ac8302782912300fb67b2208f4858b15bf8b2
vic-lee <viclee@nyu.edu>
2019-04-08 13:51:47 -0400

    Migrate methods to parent Manager

--- src/manager/manager.h
+++ src/manager/manager.h
@@ -22,6 +22,18 @@ class Manager
     task::TaskTable task_table_;
     std::vector<task::Task *> blocked_tasks_table_;
     task::ResourceTable resource_table_;
+
+    bool is_in_blocked_table(int id);
+    void remove_from_blocked_table(task::Task *t);
+
+    std::map<int, bool> create_visit_status_table_for_all_tasks();
+
+    void do_all_latest_initiates(std::map<int, bool> &visit_status);
+    void do_all_latest_terminates(std::map<int, bool> &visit_status);
+    void do_all_latest_requests(std::map<int, bool> &visit_status);
+    void do_all_latest_releases(std::map<int, bool> &visit_status);
+    void do_all_latest_activity_of_type(std::string type, std::map<int, bool> &visit_status);
+    bool do_one_latest_activity_of_type(std::string type, std::map<int, bool> &visit_status, task::Task *task, bool from_blocked);
 };
 } // namespace manager
 
